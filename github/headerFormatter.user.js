7c18a0e42c7850d9d2ed4d6071c213b29fac12b9
Reimoo <moolestable@yahoo.com>
2025-08-06 11:10:05 -0700

    Update headerFormatter.user.js

--- github/headerFormatter.user.js
+++ github/headerFormatter.user.js
@@ -3,11 +3,12 @@
 
 // @author       Reibies
 // @namespace    https://github.com/Reibies
-// @downloadURL  https://github.com/Reibies/WEB_Userscripts/raw/refs/head/master/github/headerFormatter.user.js
-// @version      0.1
+// @downloadURL  https://raw.githubusercontent.com/Reibies/WEB_Userscripts/master/github/headerFormatter.user.js
+// @updateURL    https://raw.githubusercontent.com/Reibies/WEB_Userscripts/master/github/headerFormatter.user.js
+// @version      0.3
 
 // @description  Adds a button to format and copy userscript headers based on the GitHub URL.
-// @match        https://github.com/*/*/edit/*
+// @match        https://github.com/*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
@@ -15,31 +16,74 @@
 (function() {
     'use strict';
 
-    function createFormatButton() {
-        const previewButton = document.querySelector('.BlobEditHeader-module__SegmentedControl--reacR');
-        if (previewButton) {
-            const formatButton = document.createElement('button');
-            formatButton.innerHTML = '💫 Header';
-            formatButton.className = 'btn ml-2';
-            formatButton.style.cssText = 'border: 1px solid #2da44e; color: #2da44e;';
+    const BUTTON_ID = 'userscript-header-formatter-button';
 
-            formatButton.addEventListener('click', () => {
-                const url = window.location.href;
-                const parts = url.split('/');
-                const author = parts[3];
-                const repo = parts[4];
-                const filePath = parts.slice(7).join('/');
+    const githubIconSVG = `
+        <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-table">
+            <path d="M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v12.5A1.75 1.75 0 0 1 14.25 16H1.75A1.75 1.75 0 0 1 0 14.25ZM6.5 6.5v8h7.75a.25.25 0 0 0 .25-.25V6.5Zm8-1.5V1.75a.25.25 0 0 0-.25-.25H6.5V5Zm-13 1.5v7.75c0 .138.112.25.25.25H5v-8ZM5 5V1.5H1.75a.25.25 0 0 0-.25.25V5Z"></path>
+        </svg>
+    `;
 
+    function addHeaderButton() {
+        const isEditPage = window.location.pathname.includes('/edit/');
+        const isNewPage = window.location.pathname.includes('/new/');
+
+        if (!isEditPage && !isNewPage) {
+            return;
+        }
+
+        const buttonContainer = document.querySelector('ul[aria-label="Edit mode"]');
+        if (!buttonContainer || document.getElementById(BUTTON_ID)) {
+            return;
+        }
+
+        const formatButton = document.createElement('button');
+        formatButton.innerHTML = `${githubIconSVG}<span style="margin-left: 5px;">user.js Header</span>`;
+        formatButton.id = BUTTON_ID;
+        formatButton.className = 'btn ml-2';
+
+        formatButton.addEventListener('click', () => {
+            const url = window.location.href;
+            const parts = url.split('/');
+            const author = parts[3];
+            const repo = parts[4];
+            let filePath = '';
+            let branch = 'main'; // Default to 'main'
+
+            if (isEditPage) {
+                branch = parts[6];
+                filePath = parts.slice(7).join('/');
+            } else if (isNewPage) {
+                branch = parts[6];
+
+                const pathParts = [];
+                const breadcrumbs = document.querySelectorAll('nav[data-testid="breadcrumbs"] ol li a');
+                for (let i = 1; i < breadcrumbs.length; i++) {
+                    pathParts.push(breadcrumbs[i].textContent);
+                }
+
+                const fileNameInput = document.querySelector('input[placeholder="Name your file..."]');
+                if (fileNameInput && fileNameInput.value) {
+                    pathParts.push(fileNameInput.value);
+                }
+                filePath = pathParts.join('/');
+            }
+
+            if (filePath) {
+                const rawUrl = `https://raw.githubusercontent.com/${author}/${repo}/${branch}/${filePath}`;
                 const formattedHeader = `// @author       ${author}\n` +
                                       `// @namespace    https://github.com/${author}\n` +
-                                      `// @downloadURL  https://github.com/${author}/${repo}/raw/refs/head/master/${filePath}`;
+                                      `// @downloadURL  ${rawUrl}\n` +
+                                      `// @updateURL    ${rawUrl}`;
 
                 copyToClipboard(formattedHeader);
                 alert('Userscript header copied to clipboard!');
-            });
+            } else {
+                alert('Could not determine file path. If creating a new file, please enter the file name first.');
+            }
+        });
 
-            previewButton.parentNode.insertBefore(formatButton, previewButton.nextSibling);
-        }
+        buttonContainer.parentNode.insertBefore(formatButton, buttonContainer.nextSibling);
     }
 
     function copyToClipboard(text) {
@@ -51,6 +95,15 @@
         document.body.removeChild(textarea);
     }
 
-    // Run the script
-    createFormatButton();
+
+    const observer = new MutationObserver(() => {
+        setTimeout(addHeaderButton, 100);
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+
+    addHeaderButton();
 })();
